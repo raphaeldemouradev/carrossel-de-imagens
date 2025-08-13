fe640ed9afc591ea4781308abd16a1bba68c9804
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-08-13 19:33:09 -0300

    tentativa de codar falhada

---
+++ script.js
@@ -0,0 +1,14 @@
+var proxima = document.getElementById('proxima')
+proxima.addEventListener('click', function prox() {
+    var area = document.getElementById('area')
+    var imge1 = document.getElementById('img1')
+    var imge2 = document.getElementById('img2')
+    var imge3 = document.getElementById('img3')
+
+    area.innerHTML = img2
+})
+
+
+
+var anterior = document.getElementById('anterior')
+
